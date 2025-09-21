b604e03ac6e1db5aa15b2b07a8a7ea0ec2df77d8
mrkjhm <162660469+mrkjhm@users.noreply.github.com>
2025-09-21 22:49:31 +0800

    modified cookieOptions.ts

--- src/utils/cookieOptions.ts
+++ src/utils/cookieOptions.ts
@@ -2,16 +2,16 @@ import { CookieOptions } from "express";
 
 export const accessTokenCookieOptions: CookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  secure: true,
+  sameSite: "none",
   maxAge: 15 * 60 * 1000, // 15 minutes
   path: "/",
 };
 
 export const refreshTokenCookieOptions: CookieOptions = {
   httpOnly: true,
-  secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+  secure: true,
+  sameSite: "none",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   path: "/",
 };
